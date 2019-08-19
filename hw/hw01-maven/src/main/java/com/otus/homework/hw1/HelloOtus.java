7959a89589f3ac050e1343b3f0a47cdea2d4f8a2
Kazartsev, Alexander <a.kazartsev@alibaba-inc.com>
2019-08-19 19:35:59 +0300

    Working project with guava

---
+++ hw/hw01-maven/src/main/java/com/otus/homework/hw1/HelloOtus.java
@@ -0,0 +1,10 @@
+package com.otus.homework.hw1;
+
+
+import com.google.common.primitives.Doubles;
+
+public class HelloOtus {
+    void callGuava() {
+        System.out.println(Doubles.compare(10.0, 10.0));
+    }
+}
---
+++ hw/hw01-maven/src/main/java/com/otus/homework/hw1/Hw1Application.java
@@ -0,0 +1,7 @@
+package com.otus.homework.hw1;
+
+public class Hw1Application {
+    public static void main(String[] args) {
+        new HelloOtus().callGuava();
+    }
+}
